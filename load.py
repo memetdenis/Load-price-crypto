289dd437f553c41b2a5ff7e07b1d8e4dace350c5
Memet Denis <memetdenis@ya.ru>
2022-08-31 10:34:41 +0300

    Тест отправка

---
+++ load.py
@@ -0,0 +1,31 @@
+import websocket
+import json
+import time
+import MySQLdb
+
+def on_message(ws, message):
+    time_start = time.time() #Запомним счетчик времени для замера времени выполнении кода.
+
+    data = json.loads(message)#Выгрузим JSON в массив
+
+    #Подключимся к базе данных
+    db=MySQLdb.connect(host="localhost", user="root", passwd="", db="price")
+    cursor = db.cursor()
+
+    #Переберём массив данных(json) 
+    for crypto_symbol in data:
+        cursor.execute("INSERT INTO `price` (`symbol`, `price`) VALUES ('"+crypto_symbol["s"]+"', '"+crypto_symbol["c"]+"') ON DUPLICATE KEY UPDATE price = '"+crypto_symbol["c"]+"' , last_update = UNIX_TIMESTAMP();") #Записать изменение цены
+    
+    db.commit() #Зафиксировать транзакции
+    db.close() #Закрыть подключение
+    print(f'Обработали {len(data)} торговых символов за {round(time.time()-time_start,3)} сек.')
+    #time.sleep(3) #Пауза для подумать и остыть.
+
+def on_close(ws):
+    print("### closed ###")
+
+ws = websocket.WebSocketApp("wss://stream.binance.com:9443/ws/!ticker@arr",
+                            on_message=on_message,
+                            on_close=on_close)
+
+ws.run_forever()  # Set dispatcher to automatic reconnection d
