6a9e591f4bbe8741003608574861e7567e3639ee
Memet Denis <memetdenis@ya.ru>
2022-08-31 11:12:47 +0300

    add sql create table

---
+++ price.sql
@@ -0,0 +1,22 @@
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `price`
+--
+
+CREATE TABLE `price` (
+  `symbol` varchar(20) NOT NULL,
+  `price` decimal(20,10) NOT NULL,
+  `last_modified` int(10) UNSIGNED NOT NULL DEFAULT unix_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `price`
+--
+ALTER TABLE `price`
+  ADD PRIMARY KEY (`symbol`);
+COMMIT;
