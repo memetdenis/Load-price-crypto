fea8ef6abc9530d6ee9058e2772aa4f65b4dd40d
Memet Denis <memetdenis@ya.ru>
2022-09-01 22:58:00 +0300

    Создание базы даных

--- price.sql
+++ price.sql
@@ -7,7 +7,7 @@
 CREATE TABLE `price` (
   `symbol` varchar(20) NOT NULL,
   `price` decimal(20,10) NOT NULL,
-  `last_modified` int(10) UNSIGNED NOT NULL DEFAULT unix_timestamp()
+  `last_update` int(10) UNSIGNED NOT NULL DEFAULT unix_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
