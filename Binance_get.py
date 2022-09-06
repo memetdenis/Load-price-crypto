6d95eeb250e54453c55be7e2b9d805d5416994aa
Memet Denis <memetdenis@ya.ru>
2022-09-06 20:00:52 +0300

    1.0.2 Записываем цены только больше 0

--- Binance_get.py
+++ Binance_get.py
@@ -1,28 +1,30 @@
 import time
-import requests
-import json
 import MySQLdb
+import requests
 
-#Подключимся к базе данных
+# Подключимся к базе данных
 db=MySQLdb.connect(host="localhost", user="root", passwd="", db="price")
 cursor = db.cursor()
 
-#Функция загрузки цен
-def load_price():
-    
-    time_start = time.time()
-    
-    #Получим данные в виде JSON
-    data = requests.get("https://api.binance.com/api/v3/ticker/24hr").json()
+# Функция загрузки цен с биржи GATE
+def load_Binance():
+    time_start = time.time() # Запомним время страта
 
-    #В цикле переберём каждую валюту.
-    for symbol in data:
-        cursor.execute("INSERT INTO `price` (`symbol`, `price`) VALUES ('"+symbol["symbol"]+"', '"+symbol["lastPrice"]+"') ON DUPLICATE KEY UPDATE price = '"+symbol["lastPrice"]+"' , last_update = UNIX_TIMESTAMP();") #Записать изменение цены
+    #Получим по ссылки данные с ценами.
+    price_list = requests.get("https://api.binance.com/api/v3/ticker/24hr").json() # Получим ответ в виде JSON формата
 
-    db.commit() #Зафиксировать транзакции
-    print(f"Загрузка за {round(time.time()-time_start,3)} сек.")
+    count_symbol = 0 # Счетчик торговых пар с ценами
+    # Переберём в цикле все торговые пары
+    for symbol in price_list:
+        if float(symbol['lastPrice']) > 0: # Проверим наличие цены
+            count_symbol += 1 # Увеличим счетчик торговых пар.
+            cursor.execute("INSERT INTO `price` (`symbol`, `price`) VALUES ('"+symbol['symbol']+"', '"+symbol['lastPrice']+"') ON DUPLICATE KEY UPDATE price = '"+symbol['lastPrice']+"' , last_update = UNIX_TIMESTAMP();") # Записать изменение цены
+        
+    db.commit() # После всех записей, зафиксируем записаное.
 
-#Бесконечный цикл
+    # Сообщим о проделанной работе
+    print(f"Загрузил {count_symbol} торговых пар за {round(time.time()-time_start,3)} сек.")
+
+# Сделаем бесконечный цикл загрузки цен.
 while 1==1:
-    load_price()
-    #time.sleep(1)
+    load_Binance()
