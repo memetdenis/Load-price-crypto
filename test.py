07a0968bf57bf95c354a59d17f585aa4e406b8b3
Memet Denis <memetdenis@ya.ru>
2022-09-16 17:37:39 +0300

    test

--- test.py
+++ test.py
@@ -1,36 +1,7 @@
-import tkinter
-
-def click_btn_Binance():
-    print(btn_Binance['text'])
-    btn_Binance.configure(text="Стоп")
-
-window = tkinter.Tk()
-
-frame1 = tkinter.Frame(master=window)
-frame1.pack(fill=tkinter.X)
-
-txt_Binance = tkinter.Label(master=frame1, text="Binance", width=10)
-txt_Binance.pack(side=tkinter.LEFT)
-
-txt_BinanceJob = tkinter.Label(master=frame1, text="остановлен", width=10)
-txt_BinanceJob.pack(side=tkinter.LEFT)
-
-btn_Binance = tkinter.Button(master=frame1, text="Старт", width=10, command=click_btn_Binance)
-btn_Binance.pack(side=tkinter.RIGHT)
-
-frame2 = tkinter.Frame(master=window)
-frame2.pack(fill=tkinter.X)
-
-txt_Binance2 = tkinter.Label(master=frame2, text="Gate", width=10)
-txt_Binance2.pack(side=tkinter.LEFT)
-
-img = tkinter.PhotoImage(file="img/ok_16x16.png")
-txt_BinanceJob2 = tkinter.Label(master=frame2, image=img, text="Job", width=18)
-txt_BinanceJob2.pack(side=tkinter.LEFT)
+from decimal import Decimal
 
 
-btn_Binance2 = tkinter.Button(master=frame2, text="Стоп", width=10)
-btn_Binance2.pack(side=tkinter.RIGHT)
-
-
-window.mainloop()
+old = round(float('0.00000000000017'),14)
+new = round(float(''),14)
+changes = round(((new - old) / old) * 100, 2)
+print(changes)
